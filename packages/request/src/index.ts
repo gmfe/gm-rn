39cbf3863e4e5c6a5bd952b17a534d22b32fad48
jawei <1005945485@qq.com>
2022-11-08 10:04:16 +0800

    chore: export

--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -1,9 +1,10 @@
-import { instance, Request } from './request';
+import { instance, Request } from './request'
 
-import configHeaders from './config_headers';
-import configTrace from './config_trace';
-import configError from './config_error';
-import { initAuth, clearAuth } from './init';
+import configHeaders from './config_headers'
+import configTrace from './config_trace'
+import configError from './config_error'
+import configPrivateDomain from './config_private_domain'
+import { initAuth, clearAuth } from './init'
 
 export {
   instance,
@@ -13,6 +14,7 @@ export {
   configError,
   initAuth,
   clearAuth,
-};
+  configPrivateDomain,
+}
 
-export type { Response } from './types';
+export type { Response } from './types'
