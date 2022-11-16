48b1ae84af3d412732266001ffb07e8925397599
baozouai <baozouai@gmail.com>
2022-11-16 20:56:52 +0800

    feat: cellRows增加更多prop

--- packages/components/src/cell_rows/index.tsx
+++ packages/components/src/cell_rows/index.tsx
@@ -1,42 +1,71 @@
-import React, { FC } from 'react'
+import React, { FC, ReactNode } from 'react'
 import { View, Text } from 'react-native'
 import S from '../styles'
 import FlexView, { FlexViewProps } from '../flex_view'
+import { textStyleType } from '../type'
 
-interface CellRowProps extends FlexViewProps {
+export interface CellRowProps extends FlexViewProps {
   label: string
+  children: ReactNode
+  hide?: boolean
+  labelStyle?: textStyleType
+  childrenStyle?: textStyleType
 }
 
-interface CellRowsProps extends FlexViewProps {
+export interface CellRowsProps extends FlexViewProps {
   rows: CellRowProps[]
+  commonLabelStyle?: textStyleType
+  commonChildrenStyle?: textStyleType
   isBetween?: boolean
 }
-export const CellRows: FC<CellRowsProps> = ({ rows, isBetween, ...res }) => {
+export const CellRows: FC<CellRowsProps> = ({
+  rows,
+  isBetween,
+  commonLabelStyle,
+  commonChildrenStyle,
+  ...res
+}) => {
   return (
     <FlexView {...res}>
-      {rows.map((row, index) => (
-        <CellRow
-          {...row}
-          key={row.label}
-          justifyBetween={isBetween}
-          marginBottom8={index !== rows.length - 1}
-        />
-      ))}
+      {rows
+        .filter((item) => !item.hide)
+        .map(({ labelStyle, childrenStyle, ...rest }, index, self) => (
+          <CellRow
+            {...rest}
+            key={rest.label}
+            labelStyle={[commonLabelStyle, labelStyle]}
+            childrenStyle={[commonChildrenStyle, childrenStyle]}
+            justifyBetween={isBetween}
+            marginBottom8={index !== self.length - 1}
+          />
+        ))}
     </FlexView>
   )
 }
 
-export const CellRow: FC<CellRowProps> = ({ label, children, ...res }) => {
+export const CellRow: FC<CellRowProps> = ({
+  label,
+  labelStyle,
+  childrenStyle,
+  hide,
+  children,
+  ...res
+}) => {
+  if (hide) return null
   return (
     <FlexView row {...res} marginBottom8>
       <View>
-        <Text style={[S.alignStart]}>
+        <Text style={[S.alignStart, labelStyle]}>
           {label}
           {res.justifyBetween ? null : '：'}
         </Text>
       </View>
       <FlexView row flex={!res.justifyBetween}>
-        {typeof children === 'object' ? children : <Text>{children}</Text>}
+        {typeof children === 'object' ? (
+          children
+        ) : (
+          <Text style={childrenStyle}>{children}</Text>
+        )}
       </FlexView>
     </FlexView>
   )
