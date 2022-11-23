ddd0f961517f8e9aeada94d81a6afacf350c9cfc
baozouai <baozouai@gmail.com>
2022-11-23 18:21:08 +0800

    fix: uuid导入修复

--- packages/utils/src/qiniu_upload/index.ts
+++ packages/utils/src/qiniu_upload/index.ts
@@ -1,70 +1,70 @@
-import UUID from '../uuid';
-import * as AsyncStorage from '../async_storage';
-import axios, { AxiosResponse } from 'axios';
+import { UUID } from '../uuid'
+import * as AsyncStorage from '../async_storage'
+import axios, { AxiosResponse } from 'axios'
 
-const TOKEN_KEY_BASE = 'x_qiniu_token_';
-const TOKEN_KEY_CACHE_TIME_BASE = 'x_qiniu_token_cache_time_';
+const TOKEN_KEY_BASE = 'x_qiniu_token_'
+const TOKEN_KEY_CACHE_TIME_BASE = 'x_qiniu_token_cache_time_'
 
 export interface ImageFile {
-  name: string;
-  uri: string;
-  type: string;
+  name: string
+  uri: string
+  type: string
 }
 export interface QiniuInfo {
-  prefix: string;
-  token: string;
-  expire_time: string;
+  prefix: string
+  token: string
+  expire_time: string
 }
 export interface Options {
-  domain?: string;
-  fileType: string; // 缓存用
-  getQiniuInfo: () => Promise<QiniuInfo>;
+  domain?: string
+  fileType: string // 缓存用
+  getQiniuInfo: () => Promise<QiniuInfo>
 }
 
 interface UploadData {
-  hash: string;
-  key: string;
+  hash: string
+  key: string
 }
 
 function getUploadFileName(blob: ImageFile) {
-  const { type = '', name } = blob;
-  let suf = type.includes('image') ? type.split('/').pop() : '';
+  const { type = '', name } = blob
+  let suf = type.includes('image') ? type.split('/').pop() : ''
   if (!suf) {
-    suf = name.split('.').pop();
+    suf = name.split('.').pop()
   }
 
   if (!suf) {
-    throw new Error('Can not find the suffix');
+    throw new Error('Can not find the suffix')
   }
 
-  return `${UUID.generate()}.${suf}`;
+  return `${UUID.generate()}.${suf}`
 }
 
 async function getCacheInfo(
   fetchInfo: () => Promise<QiniuInfo>,
   fileType: string,
 ) {
-  const TOKEN_INFO_KEY = TOKEN_KEY_BASE + fileType;
-  const TOKEN_KEY_CACHE_TIME = TOKEN_KEY_CACHE_TIME_BASE + fileType;
-  let info = await AsyncStorage.load(TOKEN_INFO_KEY);
-  const _cache = await AsyncStorage.loadString(TOKEN_KEY_CACHE_TIME);
-  let _cacheTime = _cache && new Date(_cache);
+  const TOKEN_INFO_KEY = TOKEN_KEY_BASE + fileType
+  const TOKEN_KEY_CACHE_TIME = TOKEN_KEY_CACHE_TIME_BASE + fileType
+  let info = await AsyncStorage.load(TOKEN_INFO_KEY)
+  const _cache = await AsyncStorage.loadString(TOKEN_KEY_CACHE_TIME)
+  let _cacheTime = _cache && new Date(_cache)
   if (info && _cacheTime && +new Date() - +_cacheTime < 5 * 60 * 1000) {
-    return info;
+    return info
   }
-  info = await fetchInfo();
-  _cacheTime = new Date();
+  info = await fetchInfo()
+  _cacheTime = new Date()
 
-  AsyncStorage.save(TOKEN_INFO_KEY, info);
-  AsyncStorage.saveString(TOKEN_KEY_CACHE_TIME, `${_cacheTime}`);
+  AsyncStorage.save(TOKEN_INFO_KEY, info)
+  AsyncStorage.saveString(TOKEN_KEY_CACHE_TIME, `${_cacheTime}`)
 
-  return info;
+  return info
 }
 
 async function uploadRequest(url: string, data: { [key: string]: any }) {
-  const formData = new FormData();
+  const formData = new FormData()
   for (const key in data) {
-    formData.append(key, data[key]);
+    formData.append(key, data[key])
   }
 
   const res = await axios({
@@ -72,9 +72,9 @@ async function uploadRequest(url: string, data: { [key: string]: any }) {
     url,
     data: formData,
     headers: { 'content-type': 'application/x-www-form-urlencoded' },
-  }).then((response: AxiosResponse<UploadData>) => response.data);
+  }).then((response: AxiosResponse<UploadData>) => response.data)
 
-  return res;
+  return res
 }
 
 export async function qiniuUpload(blob: ImageFile, opts: Options) {
@@ -83,27 +83,27 @@ export async function qiniuUpload(blob: ImageFile, opts: Options) {
     domain: opts.domain || 'https://qncdn.guanmai.cn',
     getQiniuInfo: opts.getQiniuInfo,
     fileType: opts.fileType,
-  };
+  }
 
   if (!options.getQiniuInfo) {
-    throw new Error('need getQiniuInfo');
+    throw new Error('need getQiniuInfo')
   }
 
   const { prefix, token } = await getCacheInfo(
     options.getQiniuInfo,
     options.fileType,
-  );
+  )
 
-  const name = getUploadFileName(blob);
-  const path = prefix ? `${prefix}${name}` : name;
+  const name = getUploadFileName(blob)
+  const path = prefix ? `${prefix}${name}` : name
 
   const json = await uploadRequest(options.uploadUrl, {
     file: blob,
     token,
     key: path,
-  });
+  })
 
-  const url = `${options.domain}/${json.key}`;
+  const url = `${options.domain}/${json.key}`
 
-  return { url, key: json.key };
+  return { url, key: json.key }
 }
