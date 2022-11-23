8f146785702b9b03c3cea009dc098bb71925c91e
baozouai <baozouai@gmail.com>
2022-11-23 08:49:47 +0800

    feat: 修改请求

--- packages/request/src/config_headers.ts
+++ packages/request/src/config_headers.ts
@@ -1,21 +1,18 @@
-import { instance } from './request';
-import { UUID } from '@gm-rn/utils';
+import { instance } from './request'
+import { UUID } from '@gm-rn/utils'
 
-function configHeaders(): void {
+function configHeaders(clientName = 'driver', version = '1.0.0'): void {
   // 指纹暂时使用 UUID，RN 可以使用设备识别号代替
-  let clientId = UUID.generate();
-
-  const clientName = 'driver';
-  const version = '1.0.0';
+  const clientId = UUID.generate()
 
   instance.defaults.headers.common[
     'X-Client'
-  ] = `${clientName}/${version} ${clientId}`;
+  ] = `${clientName}/${version} ${clientId}`
 
   instance.interceptors.request.use((config) => {
-    config.headers['X-Request-Id'] = UUID.generate();
-    return config;
-  });
+    config.headers['X-Request-Id'] = UUID.generate()
+    return config
+  })
 }
 
-export default configHeaders;
+export default configHeaders
--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -1,9 +1,9 @@
-import { instance, Request } from './request';
+import { instance, Request } from './request'
 
-import configHeaders from './config_headers';
-import configTrace from './config_trace';
-import configError from './config_error';
-import { initAuth, clearAuth } from './init';
+import configHeaders from './config_headers'
+import configTrace from './config_trace'
+import configError from './config_error'
+import { initAuth, clearAuth } from './init'
 
 export {
   instance,
@@ -13,6 +13,6 @@ export {
   configError,
   initAuth,
   clearAuth,
-};
+}
 
-export type { Response } from './types';
+export type { Response } from './types'
