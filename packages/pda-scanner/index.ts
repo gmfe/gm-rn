13510659c7244c5ccd5ed4e818d37fbe9cc63997
baozouai <baozouai@gmail.com>
2022-12-28 14:43:50 +0800

    feat: pda增加listen方法,启用addListener和removeListener

--- packages/pda-scanner/index.ts
+++ packages/pda-scanner/index.ts
@@ -5,18 +5,26 @@ export interface PdaScannerProps {
   initScanner: (callback: (error: string, res: string) => void) => void
   singleScan: (enable: boolean) => void
   recycleScanner: () => void
+  /** @deprecated 请使用 `listen` */
   addListener: (callback: (result: string) => void) => void
+  /** 添加 listener，会返回 clearListener 函数 */
+  listen: (callback: (result: string) => void) => () => void
+  /** @deprecated 请使用 `listen`返回的 `clernFn` */
   removeListener: () => void
 }
 
 const { PdaScanner } = NativeModules
 let eventListener: any = null
+let eventEmitter: NativeEventEmitter | null = null
 
+function initEventEmitter() {
+  if (!eventEmitter) eventEmitter = new NativeEventEmitter(PdaScanner)
+}
 const Scanner: PdaScannerProps = {
   ...PdaScanner,
   addListener: (callback) => {
-    const eventEmitter = new NativeEventEmitter(PdaScanner)
-    eventListener = eventEmitter.addListener(
+    initEventEmitter()
+    eventListener = eventEmitter!.addListener(
       PdaScanner.SCANNER_RESULT_CHANGE_KEY,
       (event: any) => {
         callback(event.result)
@@ -28,6 +36,17 @@ const Scanner: PdaScannerProps = {
       eventListener.remove()
       eventListener = null
     }
+  },
+  listen: (callback) => {
+    initEventEmitter()
+    const listener = (event: any) => {
+      callback(event.result)
+    }
+    const emitterSubscription = eventEmitter!.addListener(
+      PdaScanner.SCANNER_RESULT_CHANGE_KEY,
+      listener,
+    )
+    return emitterSubscription.remove.bind(emitterSubscription)
   },
 }
 
